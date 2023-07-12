7019cd6f933f8c3fb88b545d423408a215d197fd
shikha dhar <shikhadhar212@gmail.com>
2023-07-12 13:37:17 +0530

    submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-// your code here
+    if(num==1) return 0;
+    if(num==2) return 1;
+// calling faith 1
+let fibnm1 = fibonacci(num-1);
+//calling faith2
+let fibnm2 = fibonacci(num-2);
+return fibnm1+fibnm2;
+
 }
 
 module.exports = fibonacci;
